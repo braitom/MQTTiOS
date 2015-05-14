b44c478e59216dc5cd361c730443a06ea95277bf
braitom <braitone.tomtom@gmail.com>
2015-05-14 11:15:34 +0900

    Initial Commit

---
+++ MqttiOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MqttiOS
+//
+//  Created by kaigi on 2015/05/14.
+//  Copyright (c) 2015年 braitom. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
